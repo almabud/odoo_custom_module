19d465df1088a281f4920cadc8f50fd226213c40
root <root@localhost.localdomain>
2020-08-22 16:48:50 +0600

    Added sequence field to the model

--- odoo-custom-addons/om_hospital/__manifest__.py
+++ odoo-custom-addons/om_hospital/__manifest__.py
@@ -14,6 +14,7 @@
     'depends' : ['mail'],
     'data': [
         'security/ir.model.access.csv',
+        'data/sequence.xml',
         'views/patient.xml',
     ],
     'demo': [],
--- odoo-custom-addons/om_hospital/models/patient.py
+++ odoo-custom-addons/om_hospital/models/patient.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, _, api
 
 
 class HospitalPatients(models.Model):
@@ -9,3 +9,12 @@ class HospitalPatients(models.Model):
     age = fields.Integer(string='Age')
     notes = fields.Text(string='Notes')
     image = fields.Binary(string='Image')
+    name_seq = fields.Char(string='Hospital patient sequence', required=True, readonly=True, copy=False, index=True,
+                           default=lambda self: _('new'))
+
+    @api.model
+    def create(self, vals):
+        if vals.get('name_seq', _('new')) == _('new'):
+            vals['name_seq'] = self.env['ir.sequence'].next_by_code('hospital.patient.sequence') or _('New')
+        result = super(HospitalPatients, self).create(vals)
+        return result
