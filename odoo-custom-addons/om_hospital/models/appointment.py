c69f9edf3ea0f375bb828e6564128d8cb4c3c9ca
root <root@localhost.localdomain>
2020-08-24 15:01:28 +0600

    Add smart button in odoo

--- odoo-custom-addons/om_hospital/models/appointment.py
+++ odoo-custom-addons/om_hospital/models/appointment.py
@@ -6,17 +6,18 @@ class HospitalAppointment(models.Model):
     _description = 'Appointment'
     _inherit = ['mail.thread', 'mail.activity.mixin']
     _rec_name = 'appointment_seq'
+    _order = 'appointment_date desc'
 
     @api.model
     def create(self, vals):
         if vals.get('appointment_seq', _('New') == _('New')):
-            vals['appointment_seq'] = self.env['ir.sequence'].next_by_code('hospital.appointment') or _('New')
+            vals['appointment_seq'] = self.env['ir.sequence'].next_by_code('hospital.appointment.sequence') or _('New')
         result = super(HospitalAppointment, self).create(vals)
         return result
 
     appointment_seq = fields.Char(string='Appointment ID', required=True, copy=False, readonly=True, index=True,
                        default=lambda self: _('New'))
     patient_id = fields.Many2one('hospital.patient', string='Patient', required=True)
-    patient_age = fields.Integer('Age')
+    patient_age = fields.Integer(string='Age', related='patient_id.age')
     notes = fields.Text(stirng='Registration Note')
     appointment_date = fields.Date(string='Date', required=True)
--- odoo-custom-addons/om_hospital/models/patient.py
+++ odoo-custom-addons/om_hospital/models/patient.py
@@ -10,10 +10,26 @@ class HospitalPatients(models.Model):
     @api.depends('age_group')
     def __compute_age_group(self):
         if self.age:
-            if self.age >=18:
+            if self.age >= 18:
                 self.age_group = 'major'
             else:
                 self.age_group = 'minor'
+
+    @api.depends('appointment_count')
+    def get_appointment_count(self):
+        count = self.env['hospital.appointment'].search_count([('patient_id', '=', self.id)])
+        self.appointment_count = count
+
+    def open_patient_appointments(self):
+        return {
+            'name': _('Appointments'),
+            'domain': [('patient_id', '=', self.id)],
+            'view_tpe': 'form',
+            'res_model': 'hospital.appointment',
+            'view_id': False,
+            'view_mode': 'tree,form',
+            'type': 'ir.actions.act_window'
+        }
 
     _name = 'hospital.patient'
     _inherit = ['mail.thread', 'mail.activity.mixin']
@@ -26,6 +42,7 @@ class HospitalPatients(models.Model):
     image = fields.Binary(string='Image')
     name_seq = fields.Char(string='Sequence', required=True, readonly=True, copy=False, index=True,
                            default=lambda self: _('new'))
+    appointment_count = fields.Integer(string='Appointment', compute='get_appointment_count')
 
     @api.model
     def create(self, vals):
